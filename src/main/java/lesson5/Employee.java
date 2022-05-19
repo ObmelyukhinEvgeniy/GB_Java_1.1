b0e69795bed02132dac7aa776639b0f538d7066f
obmelyukhin <polozhitelnyy88@mail.ru>
2022-05-19 21:11:00 +0300

    lesson5.1

---
+++ src/main/java/lesson5/Employee.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String eMail;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+
+}
