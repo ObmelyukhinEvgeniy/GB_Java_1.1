d65c4ced0ff7aa5e8afb99a438e0342776994f7a
obmelyukhin <polozhitelnyy88@mail.ru>
2022-05-19 22:17:47 +0300

    lesson5.2

--- src/main/java/lesson5/Employee.java
+++ src/main/java/lesson5/Employee.java
@@ -1,6 +1,7 @@
 package lesson5;
 
 public class Employee {
+
     private String fullName;
     private String position;
     private String eMail;
@@ -8,5 +9,13 @@ public class Employee {
     private int salary;
     private int age;
 
+    public Employee(String fullName, String position, String eMail, int phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
 
 }
