4777e9974283c2ec22d966862a9f0c6bb767afc2
obmelyukhin <polozhitelnyy88@mail.ru>
2022-05-22 23:29:52 +0300

    lesson5.4

--- src/main/java/lesson5/Employee.java
+++ src/main/java/lesson5/Employee.java
@@ -5,16 +5,74 @@ public class Employee {
     private String fullName;
     private String position;
     private String eMail;
-    private int phoneNumber;
+    private String phoneNumber;
     private int salary;
     private int age;
 
-    public Employee(String fullName, String position, String eMail, int phoneNumber, int salary, int age) {
+    public Employee(String fullName, String position, String eMail, String phoneNumber, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.eMail = eMail;
         this.phoneNumber = phoneNumber;
         this.salary = salary;
+        this.age = age;
+
+    }
+
+    public String toString() {
+        return ("Full Name: " + this.fullName +
+                "; Position: " + this.position +
+                "; Email: " + this.eMail +
+                "; Phone: " + this.phoneNumber +
+                "; Salary: " + this.salary +
+                "; Age: " + this.age
+        );
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
         this.age = age;
     }
 
--- src/main/java/lesson5/ListStaff.java
+++ src/main/java/lesson5/ListStaff.java
@@ -1,11 +1,23 @@
 package lesson5;
 
+import java.sql.SQLOutput;
+
 public class ListStaff {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("John Sinigalski", "Manager", "plitka@hust.as", "+7-598-743-21-23", 40000, 32);
-        Employee employee2 = new Employee("Denni Carter", "Killer", "Knith@hust.as", "+7-987-432-77-77", 500000, 41);
-        Employee employee3 = new Employee("Jimmy Bond", "Security", "Save@hust.as", "+7-987-221-56-78", 120000, 28);
-        Employee employee4 = new Employee("Anna Prime", "Secretary", "sexbool@hust.as", "+7-987-666-63-52", 100000, 27);
-        Employee employee5 = new Employee("Violetta Ester", "Insurer", "tuheir@hust.as", "+7-987-520-78-98", 80000, 25);
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("John Sinigalski", "Manager", "plitka@hust.as", "+7-598-743-21-23", 40000, 32);
+        employees[1] = new Employee("Denni Carter", "Killer", "Knith@hust.as", "+7-987-432-77-77", 500000, 41);
+        employees[2] = new Employee("Jimmy Bond", "Security", "Save@hust.as", "+7-987-221-56-78", 120000, 28);
+        employees[3] = new Employee("Anna Prime", "Secretary", "sexbool@hust.as", "+7-987-666-63-52", 100000, 27);
+        employees[4] = new Employee("Violetta Ester", "Insurer", "tuheir@hust.as", "+7-987-520-78-98", 80000, 45);
+
+        for (int i = 0; i < employees.length; i++)
+            if (employees[i].getAge() > 40) employees[i].print();
     }
+
+
 }
+
+/*for (int i = 0; i < employees.length; i++) {
+        employees[i].print();
+        }*/
