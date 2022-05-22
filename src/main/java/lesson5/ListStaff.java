2a6c68b793fbf24660f1958015b7e8aaca191a7c
obmelyukhin <polozhitelnyy88@mail.ru>
2022-05-22 23:33:43 +0300

    lesson5.5

--- src/main/java/lesson5/ListStaff.java
+++ src/main/java/lesson5/ListStaff.java
@@ -4,15 +4,15 @@ import java.sql.SQLOutput;
 
 public class ListStaff {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        employees[0] = new Employee("John Sinigalski", "Manager", "plitka@hust.as", "+7-598-743-21-23", 40000, 32);
-        employees[1] = new Employee("Denni Carter", "Killer", "Knith@hust.as", "+7-987-432-77-77", 500000, 41);
-        employees[2] = new Employee("Jimmy Bond", "Security", "Save@hust.as", "+7-987-221-56-78", 120000, 28);
-        employees[3] = new Employee("Anna Prime", "Secretary", "sexbool@hust.as", "+7-987-666-63-52", 100000, 27);
-        employees[4] = new Employee("Violetta Ester", "Insurer", "tuheir@hust.as", "+7-987-520-78-98", 80000, 45);
+        Employee[] employeeArr = new Employee[5];
+        employeeArr[0] = new Employee("John Sinigalski", "Manager", "plitka@hust.as", "+7-598-743-21-23", 40000, 32);
+        employeeArr[1] = new Employee("Denni Carter", "Killer", "Knith@hust.as", "+7-987-432-77-77", 500000, 41);
+        employeeArr[2] = new Employee("Jimmy Bond", "Security", "Save@hust.as", "+7-987-221-56-78", 120000, 28);
+        employeeArr[3] = new Employee("Anna Prime", "Secretary", "sexbool@hust.as", "+7-987-666-63-52", 100000, 27);
+        employeeArr[4] = new Employee("Violetta Ester", "Insurer", "tuheir@hust.as", "+7-987-520-78-98", 80000, 45);
 
-        for (int i = 0; i < employees.length; i++)
-            if (employees[i].getAge() > 40) employees[i].print();
+        for (int i = 0; i < employeeArr.length; i++)
+            if (employeeArr[i].getAge() > 40) employeeArr[i].print();
     }
 
 
